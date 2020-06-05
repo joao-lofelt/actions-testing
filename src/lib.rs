affc129a2308c67c9d7117d4fe182251a2c90366
João Freire <joao@lofelt.com>
2020-06-05 18:15:38 +0200

    Adds failing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
-
-
-
-
 #[cfg(test)]
 mod tests {
     #[test]
-    fn checkingstuff() {
-
-    }
     fn it_works() {
-    
-
-        assert_eq!(2 + 2, 4);
+        assert_eq!(2 + 2, 3);
     }
 }
